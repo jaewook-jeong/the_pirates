f3b576a4a7f67116b1cc045aac6171efe33625d1
dnrlwo <dnrlwo11@gmail.com>
2021-01-21 22:47:49 +0900

    feat: first page

---
+++ src/App.style.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components';
+
+export const AppBody = styled.div`
+background: #333333 url(https://www.tpirates.com/assets/img/bg_texture.svg);
+overflow: visible;
+min-height: 100vh;
+`;
